6a5444ca21c2e1b84e142e2928659b4925607a76
coolxjay <coolxjay@gmail.com>
2018-06-27 17:03:44 +0900

    Modal

---
+++ src/pages/reservation/reservation.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angular';
+
+@IonicPage()
+@Component({
+  selector: 'page-reservation',
+  templateUrl: 'reservation.html',
+})
+export class ReservationPage {
+
+  constructor(
+		public navCtrl: NavController, 
+		public navParams: NavParams,
+		public viewCtrl: ViewController
+	) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad ReservationPage');
+  }
+	
+	dismiss() {
+		this.viewCtrl.dismiss();
+	}
+
+}
